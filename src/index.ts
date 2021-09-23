0805bc053af6622b654ca3c680adbc88891fb025
CS-5 <me@carsonseese.com>
2021-09-22 22:35:23 -0400

    We handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { handleRequest } from "./handler";
+declare var WEBHOOK_URL: string;
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
+
+async function handleRequest(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  switch (pathname) {
+    case "/api/apply":
+      const formData = await request.formData();
+
+      const email = formData.get("email");
+      const user = formData.get("ubisoftAccount");
+      const message = `A new nerd thinks he's good enough to join T3ST
+      
+Email: ${email}
+Username: ${user}`;
+
+      await fetch(WEBHOOK_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          content: message,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      return Response.redirect("https://t3st.games");
+    default:
+      return Response.redirect("https://t3st.games");
+  }
+}
